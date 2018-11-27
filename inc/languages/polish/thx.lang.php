4a981b23cb60b57919aaa3063711f681ebda735b
Ulvhedin <ulvhedin80@gmail.com>
2018-11-27 21:32:15 +0100

    Poprawione pluginy, dodane paczki z tłumaczeniem. (które trzeba dorobić)

---
+++ inc/languages/polish/thx.lang.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Author: Dark Neo
+ * Plugin: Thanks System
+ * Version: 2.5.6
+ * Plugin Language File: english
+ */
+ $l['thx_main'] = "Thank";
+$l['thx_givenby'] = "Thanked: ";
+$l['thx_thanked_count'] = "<div class=\"thx_rec\"><span class=\"author_left\">Thanks Received</span><span class=\"author_right\"><b><a href=\"thx.php?thanked={2}\" id=\"thx_thanked_{3}\">{1}</a></b></span></div>";
+$l['thx_thank_count'] = "<div class=\"thx_giv\"><span class=\"author_left\">Thanks Given</span><span class=\"author_right\"><b><a href=\"thx.php?thanks={2}\" id=\"thx_thanks_{3}\">{1}</a></b></span></div>";
+$l['thx_thanked_count_mob'] = "<span class=\"dnt ut\" title=\"Thanks Given\"><b><i class=\"fa fa-thumbs-up\"></i><a href=\"thx.php?thanked={2}\" id=\"thx_thanked_{3}\">{1}</a></b></span>";
+$l['thx_thank_count_mob'] = "<span class=\"dnt re\" title=\"Thanks Received\"><b><i class=\"fa fa-thumbs-up\"></i><a href=\"thx.php?thanks={2}\" id=\"thx_thanks_{3}\">{1}</a></b></span>";
+$l['thx_thank_details'] = "This user have received <b>{1}</b> thanks in <b>{2}</b> posts, and have given <b>{3}</b> thanks";
+$l['thx_thank_details_extra'] = "<br />You've used <b>{1}</b> thanks of <b>{2}</b> you can use today";
+$l['thx_exceed'] = "You have exceed {1} max thanks per day";
+$l['thx_remove'] = "Unthank";
+$l['thx_comma'] = ", ";
+$l['thx_dir'] = "ltr";
+$l['thx_found'] = "Thanks Founded:  ";
+$l['thx_fromid'] = "FROM ID";
+$l['thx_toid'] = "TO ID";
+$l['thx_postid'] = "POST ID";
+$l['thx_hide_text'] = "You must click thanks to see this content...";
+$l['thx_hide_register'] = "Register or login to see this content";
+$l['thx_hide_portal'] = "If you are on portal can not see this content...";
+$l['thx_button_add'] = "<i class=\"fa fa-thumbs-up\">&nbsp;</i>Thank";
+$l['thx_button_del'] = "<i class=\"fa fa-thumbs-up\" style=\"transform: rotate(180deg);\">&nbsp;</i>Unthank";
+$l['thx_button_add_nor'] = "<i class=\"fa fa-thumbs-up\">&nbsp;</i>";
+$l['thx_button_del_nor'] = "<i class=\"fa fa-thumbs-up\" style=\"transform: rotate(180deg);\">&nbsp;</i>";
+$l['thx_thankyou'] = "Thanks for this post";
+$l['thx_title'] = "MyBB Thanks System";
+$l['thx_desc'] = "Add button for thanks on posts for users in forums where plugin are activated";
+$l['thx_empty'] = "There are no thanks for this user";
+$l['thx_page_view_date'] = "View Date";
+$l['thx_system_dnt'] = "Thank You MyBB System";
+$l['thx_user'] = "User";
+$l['thx_id'] = "ID";
+$l['thx_details'] = "Post";
+$l['thx_added'] = "Thanked by";
+$l['thx_date'] = "Sended";
+$l['thanks_alert'] = '{1} thanked your message "{2}"';
+$l['myalerts_setting_thanks'] = 'Receive an alert when someone thanks my messages?';
+$l['editor_thankyou'] = 'hide until thanks';
+$l['editor_hide'] = 'hide';
+$l['thx_cant_see'] = 'You can\'t see thanks list';
+$l['thx_cant_thank'] = 'You can\'t use thanks system';
+$l['thx_not_given'] = 'There are no thanks given';
+$l['thx_not_post'] = 'There are no thanks in this post';
+$l['thx_not_received'] = 'There are no thanks received';
+$l['thx_msg_title'] = 'Thanks:';	
+$l['thx_msg_add'] = 'Thank added.';
+$l['thx_msg_remove'] = 'Thank removed.';
+$l['thx_load_list'] = 'Loading thans list...';
+$l['thx_hide_tag_title'] = 'Hide until thanks';
+$l['thx_hide_tag_content'] = 'Content to be hidden';	
+$l['thx_latest_entries'] = "{1} latest(s) thank(s)";
+$l['thx_latest_entries_view_all'] = "View all";
+$l['thx_view_adduid'] = 'Viewing thanks given';
+$l['thx_view_uid'] = 'Viewing thanks received';
+$l['thx_view_pid'] = 'Viewing thanks in this post';
+$l['thx_wol'] = 'Viewing <a href="{1}" target="_blank">{2}</a>';
+$l['thx_better_response'] = 'Better response on post {1}';
+$l['thx_antiflood'] = 'You must wait {1} secconds to perform this action';
+$l['thx_meter'] = 'Thanks Meter';
+$l['thx_mail_subject'] = "Someone has thanked your post: {1} on {2}";
+$l['setting_thx_promotion'] = 'Thanks Count';
+$l['setting_thx_promotion_desc'] = 'Enter the number of thankys required. Thank count must be selected as a required value for this to be included. Select the type of comparison for thanks system.';
+$l['thx_mail_message'] = "Hi {1}:
+
+Somone has thanked to you on your post {2} on {3}
+			
+Go to the link below to see it or delete this message if not necessary to know it....
+	
+{4}/showthread.php?pid={5}
+		
+From our boards: {3}";
+$l['thx_send_mp_subject'] = " thanked your post: {1}";
+$l['thx_send_mp'] = "Dear [b]{2}[/b]:
+
+[b]{1}[/b] has thanked your post: {3}
+
+Go to the link below everytime you need to know what post have been thanked by this user, or if you prefer save this message to see it any other day.
+
+Thanks: {4}";
+$l['thx_found'] = "Thanks Founded:  ";
+$l['thx_error1'] = "There are no results for that search terms due:<br />The terms of search are invalid.";
+$l['thx_error2'] = "<br />That user have no thanks yet.";
+$l['thx_error3'] = "<br />That user do not give any thank yet.";
+$l['thx_error4'] = "<br />That post do not have any thanks.";
+$l['thx_error5'] = "<br />Try again with best search terms...";
+$l['thx_error6'] = "You must set at least one field to search for thanks...";
+$l['thx_a_moment'] = 'a moment ago';
+$l['thx_extra'] = ' ago';
+$l['thx_extra_pref'] = '';
+$l['thx_extras_month'] = '';
+$l['thx_extras'] = 's ago';
+$l['thx_time'] = ' since {1} ';
+$l['thx_year'] = 'year';
+$l['thx_month'] = 'month';
+$l['thx_day'] = 'day';
+$l['thx_hour'] = 'hour';
+$l['thx_minute'] = 'minute';
+$l['thx_second'] = 'second';
+$l['thx_newpoints'] = '<table border="0" align="center" width="215">
+<tr>
+	<td valign="middle" align="left">
+		<span style="border-bottom: 1px dashed; cursor: help;" title="Whe you thank to users messages">Thank Given</span>
+	</td>
+	<td valign="middle" align="right">{1}</td>
+</tr>
+<tr>
+	<td valign="middle" align="left">
+		<span style="border-bottom: 1px dashed; cursor: help;" title="When someone has thanked to my posts">Thanks Received</span>
+	</td>
+	<td valign="middle" align="right">{2}</td>
+</tr>
+</table>';
