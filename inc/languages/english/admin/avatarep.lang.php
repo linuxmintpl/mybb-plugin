9717d37e5032fce0c4bce20a50ca7e5c78387474
ciastek1981 <ciatek_mint@aol.com>
2018-12-02 22:58:03 +0100

    Awatary przy ostatnich postach https://community.mybb.com/mods.php?action=view&pid=74

---
+++ inc/languages/english/admin/avatarep.lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+*  Language English
+*  Last Poster Avatar on Threads and Forums v 2.8.6
+*
+*  Sitio Web: http://forosmybb.es
+*  Autor: Dark Neo - neogeoman@gmail.com
+*/
+
+// Don't remove this values
+$l['avatarep_name'] = "Shows Avatar on Threadlist and Forumlist";
+$l['avatarep_descrip'] = "Shows lastposter avatar on threadlist and forumlist and owners thread on threadlist.";
+$l['avatarep_config'] = "Plugin Configuration";
+
+// Use this to change in your language phrases
+$l['avatarep_title'] = "[Plugin] Shows Avatar on Threads and forums";
+$l['avatarep_title_descrip'] = "Configure this plugin (avatar options)";
+
+$l['avatarep_power'] = "Enable/Disable Plugin";
+$l['avatarep_power_descrip'] = "Select if you want to activate this plugin";
+
+$l['avatarep_forum'] = "Avatar on Forums";
+$l['avatarep_forum_descrip'] = "Shows lastposter avatar on forumlist";
+
+$l['avatarep_thread_owner'] = "Avatar on Threads";
+$l['avatarep_thread_owner_descrip'] = "Shows owners avatar on threadlist";
+
+$l['avatarep_thread_lastposter'] = "Avatar on Threads";
+$l['avatarep_thread_lastposter_descrip'] = "Shows lastposter avatar on threadlist";
+
+$l['avatarep_thread_announcements'] = "Avatar on Announcements";
+$l['avatarep_thread_announcements_descrip'] = "Shows avatar of announcements user";
+
+$l['avatarep_portal'] = "Avatar in Portal - Latest Threads";
+$l['avatarep_portal_descrip'] = "Shows avatar of announcements user in Portal";
+
+$l['avatarep_search'] = "Avatar on Search";
+$l['avatarep_search_descrip'] = "Shows avatar on search results";
+
+$l['avatarep_menu'] = "Modal Box popup";
+$l['avatarep_menu_descrip'] = "Shows a nice popup menu for mouse events selected down for every avatar of user";
+
+$l['avatarep_menu_events'] = "Mouse events";
+$l['avatarep_menu_events_descrip'] = "Select mouse event to show modal box.";
+
+$l['avatarep_guests'] = "Guests Avatar";
+$l['avatarep_guests_descrip'] = "Shows default avatar for guests";
+
+$l['avatarep_format'] = "Display formatted usernames";
+$l['avatarep_format_descrip'] = "Shows format names for every usergroup with html code configured";
+
+$l['avatarep_private'] = "Avatar in Private Messages";
+$l['avatarep_private_descrip'] = "Shows avatar in Private Messages";
+
+$l['avatarep_latest_threads'] = "Avatar in Latest Threads";
+$l['avatarep_latest_threads_descrip'] = "Shows avatar in Portal and SideBoxes plugin for latest threads";
+
+$l['avatarep_thread_lastposter_mark'] = "Mark replied threads";
+$l['avatarep_thread_lastposter_mark_descrip'] = "Show avatar hover thread owner when you have replied to a thread, (requires addittional queries).";
+
+$l['avatarep_thread_contributor'] = "Show Avatar Contributor";
+$l['avatarep_thread_contributor_descrip'] = "Shows avatar of thread owner in showthread, but yours if you've replied to that thread.";
+
+$l['avatarep_usercp'] = "Avatar in User Control Panel";
+$l['avatarep_usercp_descrip'] = "Shows avatar on listed thread into your user cp";
+
+$l['avatarep_onerror'] = "Shows white or broken avatars as default";
+$l['avatarep_onerror_descrip'] = "If some error ocurrs when avatar is loading like img was removed or broken, then default avatar appears (This brokes w3c validation due onerror img tag js function is not validated there, Disable this option to validate with w3c).";
