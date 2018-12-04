a437775fcb2ad6548dcc83bb4ae21a3af6817ce4
Ulvhedin <ulvhedin80@gmail.com>
2018-12-04 19:15:24 +0100

    Tłumaczenie dla recentpost plugin

---
+++ inc/languages/polish/recentpostsindex.lang.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * MyBB: Recent Posts Forum Index
+ *
+ * File: recentpostsindex.lang.php
+ * 
+ * Authors: Ulvhedin
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.1
+ * 
+ */
+
+$l['recentpostname'] = 'Najnowsze statystyki wiadomości';
+$l['recentpoststitle'] = "Ostatnie wiadomości";
+$l['poster'] = 'Opublikowany przez';
+$l['lastposttime'] = 'Czas publikacji';
+$l['postforum'] = 'Forum';
+
+?>
