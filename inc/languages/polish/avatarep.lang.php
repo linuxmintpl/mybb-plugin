af18bb1d92414b27a063f4b75999e3e9bff1d4cd
Ulvhedin <ulvhedin80@gmail.com>
2018-12-04 18:47:09 +0100

    Dodane nowe tłumaczenie dla avatarep

--- inc/languages/polish/avatarep.lang.php
+++ inc/languages/polish/avatarep.lang.php
@@ -3,8 +3,8 @@
 *  Język polski
 *  Last Poster Avatar 3.0
 *
-*  WWW: https://webboard.pl/member.php?action=profile&uid=41959
-*  Autor: Axwell
+*  WWW: https://forum.linuxmint.pl
+*  Autor: Ulvhedin
 */
 
 $l['avatarep_user_profile'] = "Pokaż profil";
@@ -13,3 +13,8 @@ $l['avatarep_user_error'] = " Błąd ";
 $l['avatarep_user_error_text'] = " Musisz się zalogować, aby zobaczyć tę zawartość ";
 $l['avatarep_user_alt'] = "Awatar użytkownika {1}";
 $l['avatarep_user_no_avatar'] = "Brak awatara";
+$l['avatarep_user_messages'] = "Pokaż wiadomości";
+$l['avatarep_user_sendemail'] = "Wyślij e-mail";
+$l['avatarep_user_threads'] = "Pokaż wątki";
+$l['avatarep_retrieving'] = "Pobieram dane";
+$l['avatarep_loading'] = "Ładuję...";
