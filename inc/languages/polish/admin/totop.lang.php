b78bc5fed915fb06856b112b9110482c727ac512
Pawlo <18744163+Pavvlik@users.noreply.github.com>
2019-02-10 21:48:39 +0100

    Dodanie pliku języka polskiego do ToTop plugin

---
+++ inc/languages/polish/admin/totop.lang.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * MyBB: [jQuery] Appearing Scroll to Top
+ *
+ * File: totop.lang.php
+ * 
+ * Authors: Vintagedaddyo, effone
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.1
+ * 
+ */
+
+// plugin_info
+
+$l['totop_Name'] = '[jQuery] Appearing Scroll to Top';
+$l['totop_Desc'] = 'Adds a scroll to top';
+$l['totop_Web'] = 'http://community.mybb.com/user-6029.html';
+$l['totop_Auth'] = 'Vintagedaddyo';
+$l['totop_AuthSite'] = 'http://community.mybb.com/user-6029.html';
+$l['totop_Ver'] = '1.1';
+$l['totop_GUID'] = '';
+$l['totop_Compat'] = '18*';
+
+//
+$l['totop_settings_Title'] = '[jQuery] Appearing Scroll to Top';
+$l['totop_settings_Description'] = 'Settings For [jQuery] Appearing Scroll to Top';
+
+//
+$l['totop_option_1_Title'] = 'Do you want to enable [jQuery] Appearing Scroll to Top Plugin?';
+$l['totop_option_1_Description'] = 'If you set this option to yes, this plugin be active on your board.';
+
+//
+$l['totop_option_2_Title'] = 'Enable plugin on Index';
+$l['totop_option_2_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_3_Title'] = 'Enable plugin on Forum Display';
+$l['totop_option_3_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_4_Title'] = 'Enable plugin on Show Thread';
+$l['totop_option_4_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_5_Title'] = 'Enable plugin on Memberlist';
+$l['totop_option_5_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_6_Title'] = 'Enable plugin on Member';
+$l['totop_option_6_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_7_Title'] = 'Enable plugin on Misc';
+$l['totop_option_7_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_8_Title'] = 'Enable plugin on Search';
+$l['totop_option_8_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_9_Title'] = 'Enable plugin on ModCP';
+$l['totop_option_9_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_10_Title'] = 'Enable plugin on UserCP';
+$l['totop_option_10_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_11_Title'] = 'Enable plugin on Private';
+$l['totop_option_11_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_12_Title'] = 'Enable plugin on Print Thread';
+$l['totop_option_12_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_13_Title'] = 'Enable plugin on Calendar';
+$l['totop_option_13_Description'] = 'Turn plugin (ON / OFF)';
+
+//
+$l['totop_option_14_Title'] = 'Enable plugin on Portal';
+$l['totop_option_14_Description'] = 'Turn plugin (ON / OFF)';
+
+?>
