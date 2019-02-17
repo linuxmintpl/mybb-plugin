8c418749450c3ff15066787ca60d57efecf27a9c
dedito <dedito@linuxmint.pl>
2019-02-17 15:25:32 +0100

    Polonizacja abp solve it

--- inc/languages/polish/admin/abp_solveit.lang.php
+++ inc/languages/polish/admin/abp_solveit.lang.php
@@ -1,26 +1,26 @@
 <?php
 $l['abp_siname'] = 'ABP Solve It !';
-$l['abp_sidesc'] = 'A way to mark a thread solved';
-$l['abp_sigs'] = 'ABP Solve It ! - Global settings';
-$l['abp_sigsd'] = 'Select global options for ABP Solve It !';
-$l['abp_sis1_tit'] = 'Forums';
-$l['abp_sis1_desc'] = 'Forums where the plugin is active';
-$l['abp_sis2_tit'] = 'Groups';
-$l['abp_sis2_desc'] = 'Member groups allowed to solve a thread';
-$l['abp_sis3_tit'] = 'Owner can choose solution';
-$l['abp_sis3_desc'] = 'Can the owner of the thread choose the solution even if he is not in a authorized group ?';
-$l['abp_sis4_tit'] = 'Close thread';
-$l['abp_sis4_desc'] = 'Is the thread closed when a solution is given ?';
-$l['abp_sis5_tit'] = 'Prefix to apply';
-$l['abp_sis5_desc'] = 'Apply a prefix to the thread ?';
+$l['abp_sidesc'] = 'Sposób oznaczania wątku jako rozwiązany';
+$l['abp_sigs'] = 'ABP Solve It ! - Ustawienia globalne';
+$l['abp_sigsd'] = 'Ustawienia globalne wtyczki ABP Solve It !';
+$l['abp_sis1_tit'] = 'Fora';
+$l['abp_sis1_desc'] = 'Fora na których wtyczka jest aktywna';
+$l['abp_sis2_tit'] = 'Grupy';
+$l['abp_sis2_desc'] = 'Członkowie grup uprawnieni do wybierania rozwiązania wątku';
+$l['abp_sis3_tit'] = 'Autor może wybierać rozwiązanie';
+$l['abp_sis3_desc'] = 'Czy autor wątku może wybieraćć rozwiązanie jeśli nie jest w uprawnionej grupie ?';
+$l['abp_sis4_tit'] = 'Zamknij wątek';
+$l['abp_sis4_desc'] = 'Czy zamykać wątek po oznaczeniu rozwiązania ?';
+$l['abp_sis5_tit'] = 'Prefix do dodania';
+$l['abp_sis5_desc'] = 'Dodać prefix do wątku ?';
 
-$l['abp_sijs1'] = 'Mark as solution';
-$l['abp_sijs2'] = 'Go to solution';
+$l['abp_sijs1'] = 'Oznacz jako rozwiązanie';
+$l['abp_sijs2'] = 'Idź do rozwiązania';
 
 // ADMIN PAGE
-$l['abp_siadmin_action'] = 'Fine tuning for ABP Solve It !';
-$l['abp_siadmin_form'] = 'Change tuning options';
-$l['abp_siadmin_save'] = 'Save';
-$l['abp_siadmin_noforum'] = 'No forum can be solved';
-$l['abp_siadmin_updok'] = 'Your preferences are up to date';
-$l['abp_siadmin_rempfx'] = 'Remove prefix';
+$l['abp_siadmin_action'] = 'Konfiguracja preferencji ABP Solve It !';
+$l['abp_siadmin_form'] = 'Zmiana konfiguracji preferencji';
+$l['abp_siadmin_save'] = 'Zapisz';
+$l['abp_siadmin_noforum'] = 'Żadne forum nie może zostać rozwiązane';
+$l['abp_siadmin_updok'] = 'Zaktualizowano preferencje';
+$l['abp_siadmin_rempfx'] = 'Usuń prefix';
