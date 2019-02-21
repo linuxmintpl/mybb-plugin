2c255626a5447a220f1bc82e20e9ff437b75cd2f
dedito <dedito@linuxmint.pl>
2019-02-21 17:35:55 +0100

    Wtyczka last posts pliki językowe polish

---
+++ inc/languages/polish/admin/last.lang.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * MyBB: Last Posts Plugin for MyBB
+ *
+ * File: last.lang.php
+ * 
+ * Authors: Hamed & updated by Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.2
+ * 
+ */
+
+// repost_plugin_info
+
+$l['repost_Name'] = 'Last Post';
+$l['repost_Desc'] = 'Show Last posts via the header of index and portal pages';
+$l['repost_Web'] = 'http://community.mybb.com/user-6029.html';
+$l['repost_Auth'] = 'Hamed & updated by Vintagedaddyo';
+$l['repost_AuthSite'] = 'http://ghasemzadeh.ir';
+$l['repost_Ver'] = '1.0.2';
+$l['repost_GUID'] = '';
+$l['repost_Compat'] = '18*';
+
+// Settings group
+
+$l['repost_name_0'] = "repost";
+$l['repost_title_0'] = "Recent Post";
+$l['repost_desc_0'] = "Settings for the recent posts plugin.";
+
+// Settings 1
+
+$l['repost_name_1'] = "showrepost";
+$l['repost_title_1'] = "Enable recent post";
+$l['repost_desc_1'] = "Display the recent posts in the forum header?";
+
+// Settings 2
+
+$l['repost_name_2'] = "countreposts";
+$l['repost_title_2'] = "Count of last posts";
+$l['repost_desc_2'] = "Enter how many posts should be shown.";
+
+// Settings 3
+
+$l['repost_name_3'] = "show_starter_repost";
+$l['repost_title_3'] = "Show Starter";
+$l['repost_desc_3'] = "Do you want to display thread starter?";
+
+// Settings 4
+
+$l['repost_name_4'] = "show_lastposter_repost";
+$l['repost_title_4'] = "Show Last poster";
+$l['repost_desc_4'] = "Do you want to display last poster?";
+
+// Settings 5
+
+$l['repost_name_5'] = "show_replys_repost";
+$l['repost_title_5'] = "Show Replies";
+$l['repost_desc_5'] = "Do you want to display replies count?";
+
+// Settings 6
+
+$l['repost_name_6'] = "show_forumname_repost";
+$l['repost_title_6'] = "Show Forum Name";
+$l['repost_desc_6'] = "Do you want to display forum name?";
+
+// Settings 7
+
+$l['repost_name_7'] = "show_views_repost";
+$l['repost_title_7'] = "Show Views";
+$l['repost_desc_7'] = "Do you want to display views count?";
+
+// Settings 8
+
+$l['repost_name_8'] = "show_time_repost";
+$l['repost_title_8'] = "Show Time";
+$l['repost_desc_8'] = "Do you want to display post time?";
+
+// Settings 9
+
+$l['repost_name_9'] = "not_show_post";
+$l['repost_title_9'] = "Hidden Forums";
+$l['repost_desc_9'] = "Enter the forums IDs that we do not show last post of them. Separate them with a comma (,).";
+
+// Settings 11
+
+$l['repost_name_11'] = "show_users";
+$l['repost_title_11'] = "Show Just For User";
+$l['repost_desc_11'] = "If this part is activated just users can see it.";
+
+?>
---
+++ inc/languages/polish/last.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * MyBB: Last Posts Plugin for MyBB
+ *
+ * File: last.lang.php
+ * 
+ * Authors: Hamed & updated by Vintagedaddyo
+ *
+ * MyBB Version: 1.8
+ *
+ * Plugin Version: 1.2
+ * 
+ */
+
+$l['last_posts'] = "Last Posts";
+$l['Starter'] = "Starter";
+$l['no_thread'] = "No Threads";
+$l['latest_threads'] = "Latest Threads";
+$l['latest_threads_replies'] = "Replies:";
+$l['latest_threads_views'] = "Views:";
+$l['latest_threads_lastpost'] = "Last Post by:";
+$l['Forum_Name'] = "Forum name:";
+$l['views'] = "Views:";
+$l['re_time'] = "Time:";
+$l['Not_read'] = "Go to first unread post";
+
+?>
