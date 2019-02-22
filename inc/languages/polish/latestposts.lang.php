0e5d75ea16e09bff54bcdbd0db899005a4c99aea
dedito <dedito@linuxmint.pl>
2019-02-22 18:19:29 +0100

    Wtyczka lastestposts

---
+++ inc/languages/polish/latestposts.lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Latest posts sidebar
+ *
+ * Website: http://mybebb.org
+ * Skype: daniel_mit1
+ *
+ **/
+ 
+ $l['latest_posts_title'] = "Ostatni post w wątku";
+ $l['latest_post_by'] = " napisany przez";
+ $l['latestposttime'] = " opublikowany {1}";
