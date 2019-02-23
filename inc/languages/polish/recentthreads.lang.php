8bb58e58ff0133a459045d5a57004e6dbc389a28
dedito <dedito@linuxmint.pl>
2019-02-23 09:34:11 +0100

    Polonizacja recentthreads 95%

--- inc/languages/polish/recentthreads.lang.php
+++ inc/languages/polish/recentthreads.lang.php
@@ -1,10 +1,10 @@
 <?php
 require_once "admin/recentthreads.lang.php";
-$l['recentthreads_recentthreads'] = "Recent Threads";
-$l['recentthreads_thread'] = "Thread";
+$l['recentthreads_recentthreads'] = "Ostatnie wiadomości";
+$l['recentthreads_thread'] = "Wątek";
 $l['recentthreads_forum'] = "Forum";
-$l['recentthreads_author'] = "Author";
-$l['recentthreads_posts'] = "Posts";
-$l['recentthreads_last_post'] = "Last Post";
-$l['recentthread_show'] = "Show the most recent threads on the index.";
-$l['recentthread_create_date'] = "Created on {1} at {2}";
+$l['recentthreads_author'] = "Autor";
+$l['recentthreads_posts'] = "Wiadomości";
+$l['recentthreads_last_post'] = "Ostatnia wiadomość";
+$l['recentthread_show'] = "Pokaż ostatnie wiadomości na stronie startowej.";
+$l['recentthread_create_date'] = "Utworzony {1} o {2}";
